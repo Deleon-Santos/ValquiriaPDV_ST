ed9a6bed44fad5f52427349ce3fc20bde553112e
Deleon <delps.santos1987@gmail.com>
2026-01-24 19:03:51 -0300

    inicio de db oline

--- db/db.py
+++ db/db.py
@@ -1,8 +1,23 @@
-
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("sqlite:///db/valquiria.db", echo=False)
+DATABASE_URL = st.secrets.get("DATABASE_URL") 
+if not DATABASE_URL:
+
+    st.error("DATABASE_URL não configurada!")
+    st.stop()
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
+)
+
+# engine = create_engine("sqlite:///db/valquiria.db", echo=False)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
